7eba3e7f46ed480d486c109eb2476a46e418a89c
Kay@thinkpad <kayfelicities@outlook.com>
2025-01-20 11:09:03 +0800

    update

--- changeView.py
+++ changeView.py
@@ -1,6 +1,9 @@
 
+content = ""
+
 for line in open('land/py_9.ini', encoding='utf-8'):
     if not line.startswith("VIEW_RECT="):
+        content += line
         continue
     print(line)
     x,y,w,h = line[len("VIEW_RECT="):].split(',')
@@ -10,4 +13,8 @@ for line in open('land/py_9.ini', encoding='utf-8'):
     h = int(h)
     print(x,y,w,h)
     x = x * 1280/720
+    w = w * 1280/720
+    content += "VIEW_RECT=%d,%d,%d,%d\n" % (x,y,w,h)
     
+
+open('land/py_9.ini', 'w', encoding='utf-8').write(content)
